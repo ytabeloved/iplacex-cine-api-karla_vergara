95797cfcf614ee0d441246983ef657207429bf09
Karla Vergara <karlisvaldi@gmail.com>
2026-05-18 23:36:51 -0400

    Use environment variable for Mongo

--- src/common/db.js
+++ src/common/db.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const uri = "mongodb://developer:hotCnuDhxWAypnCE@ac-mgu30td-shard-00-00.ifewi73.mongodb.net:27017,ac-mgu30td-shard-00-01.ifewi73.mongodb.net:27017,ac-mgu30td-shard-00-02.ifewi73.mongodb.net:27017/cine-db?ssl=true&replicaSet=atlas-btniy4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=cluster-express";
+const uri = process.env.MONGO_URI;
 
 export const client = new MongoClient(uri, {
   serverApi: {
@@ -12,20 +12,12 @@ export const client = new MongoClient(uri, {
 
 export async function connectDB() {
   try {
-
     await client.connect();
-
     await client.db("admin").command({ ping: 1 });
-
     console.log("Conexión exitosa a MongoDB Atlas");
-
     return true;
-
   } catch (error) {
-
     console.error("Error al conectar a MongoDB Atlas:", error);
-
     return false;
-
   }
 }
